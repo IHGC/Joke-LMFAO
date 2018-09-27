f6230a3424292761d7c4a9c23f359348f2c42830
Cristian Tejedor <Tejedor92@gmail.com>
2018-09-27 14:08:11 +0200

    imagen lista,cambio de layout,explorer y login

--- routes/search.js
+++ routes/search.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Joke = require("../models/Joke");
 const User = require("../models/User");
 const { isRatedByUser, addMarkTag,isOwner } = require("../middlewares/helpers");
+
 router.post("/", (req, res, next) => {
   const { search } = req.body;
   User.find({ username: { $regex: `.*${search}.*` } })
@@ -19,23 +20,14 @@ router.post("/", (req, res, next) => {
     .populate("userId")
     .then(jokes => {
       if(req.user){
-        console.log(req.user.id)
-        console.log(isOwner(jokes,req.user.id))
         jokes=isRatedByUser(jokes,req.user.id)
         jokes=isOwner(jokes,req.user.id)
       }
-      // console.log(jokes)
       jokes=addMarkTag(jokes,search)
 
       res.render("search", { jokes, search });
     });
   })
-});
-
-router.post("/:id/delete", (req, res, next) => {
-  User.find().then(users => {
-    res.render("list/new", { users });
-  });
 });
 
 module.exports = router;
