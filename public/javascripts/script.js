3e7019e5c6407a6610a562820f4696fb44db573c
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-09-28 11:51:50 +0200

    fixed merge rating stars

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,17 +5,20 @@
     precision: .5,
     maxValue: 5,
     minValue: 0,
-    rating: 0
+    rating: this.dataset||0
   }).on("rateyo.set",(e,data)=>{
     console.log($(e.target).prev())
     const {origin}=window.location
     const id=e.target.dataset.jokeId
     const rate=data.rating
+    console.log(id,e.target.dataset)
     axios.post(`${origin}/rate/${id}`,{rate})
     .then(r=>{
-      $(e.target).prev().text(r.data.rate)
-      console.log(this)
+      console.log($(e.target).parent().parent().parent().find('.rateAvg'))
+      $(e.target).parent().parent().parent().find('.rateAvg').text(r.data.rate)
+      $(e.target).parent().find('.rate').text(rate)
       console.log(r)
+     
     }).catch(e=>{
       console.log(e);
       alert(e)
